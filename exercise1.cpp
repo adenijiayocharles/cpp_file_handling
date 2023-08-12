57ced0d52b2409072a8d14f89f645b997ad9bc72
Adeniji Charles Ayodipupo <adenijiayocharles@gmail.com>
2023-08-12 10:24:18 +0100

    added dynamic file path resolution

--- exercise1.cpp
+++ exercise1.cpp
@@ -1,23 +1,31 @@
 // Question: Write a C++ program to create a new text file and write some text into it.
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 
 using namespace std;
+using std::filesystem::current_path;
 
 int main()
 {
     ofstream myFile;
-    string filePath = "/Users/adenijiayocharles/Documents/programming/cpp-projects/exercises/file_handling/exercise1.txt";
+
+    // get the current working directory
+    string currentDirectory = current_path();
+    string fileName = "/exercise1.txt";
+    string file = currentDirectory + fileName;
+
     // open a new file with the ofstream file handler
-    myFile.open(filePath);
+    myFile.open(file);
 
     // check if the file is opened
     // write text into the file
     if (myFile.is_open())
     {
-        myFile << "Charles Adeniji: This is the first exercise in file handling";
+        myFile << "Charles Ayodipupo Adeniji: This is the first exercise in file handling";
         // close the file
         myFile.close();
+        cout << "file written to and closed";
     }
     else
     {
